c2c2a98135297a771b370f500f0f26cde1272c24
Jean <jccedre@gmail.com>
2018-10-22 11:59:52 -0400

    Wrapping Up Axios tutorial

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
+import axios from '../../axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -13,11 +14,12 @@ class Blog extends Component {
     this.state = {
       posts: [],
       selectedPostId: null,
+      error: false,
     };
   }
 
   componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
+    axios.get('/posts')
     .then(response => {
       const posts = response.data.slice(0, 4);
       const updatedPosts = posts.map((post) => {
@@ -29,6 +31,8 @@ class Blog extends Component {
       this.setState({
         posts: updatedPosts,
       });
+    }).catch(error => {
+      this.setState({error: true});
     });
   }
 
@@ -36,13 +40,17 @@ class Blog extends Component {
     this.setState({selectedPostId: id});
   }
     render () {
-      const posts = this.state.posts.map((post) => {
+      let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>;
+
+      if (!this.state.error) {
+        posts = this.state.posts.map((post) => {
         return <Post
           key={post.id}
           title={post.title}
           author={post.author}
           clicked={() => this.postSelectedHandler(post.id)}/>
       });
+      }
         return (
             <div>
                 <section className="Posts">
