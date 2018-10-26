daa67cb72632aab6bdb7d5ad681dabf2e5e73d6e
Jean Carlos Cedre <jccedre@gmail.com>
2018-10-25 21:55:13 -0400

    Removed node_modules from repo

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
     render () {
@@ -22,7 +26,7 @@ class Blog extends Component {
               {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
 
               <Switch>
-                <Route path="/new-post" component={NewPost} />
+                <Route path="/new-post" component={AsyncNewPost} />
                 <Route path="/posts" component={Posts} />
                 {/* <Redirect from="/" to="/posts" /> */}
                 <Route render={() => <h1>Not found</h1>}/>
