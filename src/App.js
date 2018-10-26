c923ed2599d8c80613251d941925b0bcf3f20839
Jean <jccedre@gmail.com>
2018-10-26 09:32:52 -0400

    Feature: Learning how to set basepath

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      // <BrowserRouter basename="/my-app"> -> is used to set a base path.
       <BrowserRouter>
         <div className="App">
           <Blog />
